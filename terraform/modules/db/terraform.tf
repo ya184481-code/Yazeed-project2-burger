d82225068f1043597ec487d0cdf39ef7fd8a85ab
ibrahim <ksaatb3@gmail.com>
2025-10-09 15:10:13 +0300

    new pipline5

--- terraform/modules/db/terraform.tf
+++ terraform/modules/db/terraform.tf
@@ -1,25 +1,26 @@
+# --- MSSQL server setup ---
 resource "azurerm_mssql_server" "products_db_server" {
   name                          = "${lower(var.resource_prefix)}-db-server-${lower(replace(var.author, " ", "-"))}"
   resource_group_name           = var.rg_name
   location                      = var.rg_location
-  version                       = "15.0"
+  version                       = "12.0"
   administrator_login           = var.db_admin
   administrator_login_password  = var.db_password
+  depends_on                    = [var.rg_id]
   public_network_access_enabled = false
 }
 
-# --- Database setup ---
+# --- DB setup ---
 resource "azurerm_mssql_database" "products_db" {
   name                        = "${lower(var.resource_prefix)}-db-${lower(replace(var.author, " ", "-"))}"
   server_id                   = azurerm_mssql_server.products_db_server.id
+  depends_on                  = [azurerm_mssql_server.products_db_server]
   collation                   = "SQL_Latin1_General_CP1_CI_AS"
   max_size_gb                 = 4
   sample_name                 = "AdventureWorksLT"
   sku_name                    = "GP_S_Gen5_1" # Serverless
   auto_pause_delay_in_minutes = 60
   min_capacity                = 0.5
-
-  depends_on = [azurerm_mssql_server.products_db_server]
 }
 
 # --- Private DNS Zone ---
@@ -27,15 +28,13 @@ resource "azurerm_private_dns_zone" "sql_zone" {
   name                = "privatelink.database.windows.net"
   resource_group_name = var.rg_name
 }
-
-# --- Private DNS Zone link to VNet ---
+# --- Private DNS Zone link to Vnet ---
 resource "azurerm_private_dns_zone_virtual_network_link" "pdns_vnet_link" {
   name                  = "pdns-vnet-link"
   resource_group_name   = var.rg_name
   private_dns_zone_name = azurerm_private_dns_zone.sql_zone.name
   virtual_network_id    = var.vnet_id
 }
-
 # --- Private Endpoint ---
 resource "azurerm_private_endpoint" "sql_pe" {
   name                = "sql-private-endpoint"
@@ -55,5 +54,4 @@ resource "azurerm_private_endpoint" "sql_pe" {
     private_dns_zone_ids = [azurerm_private_dns_zone.sql_zone.id]
   }
 
-  depends_on = [azurerm_mssql_server.products_db_server]
 }
